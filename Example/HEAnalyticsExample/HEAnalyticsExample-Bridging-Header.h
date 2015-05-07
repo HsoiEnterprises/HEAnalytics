71e8af89e7419f049782842d688e512669b5622b
John C. Daub <hsoi@hsoi.com>
2015-05-07 05:51:40 -0500

    Adding HEAnalytics to the project, and a bridging header

---
+++ Example/HEAnalyticsExample/HEAnalyticsExample-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  HEAnalyticsExample-Bridging-Header.h
+//  HEAnalyticsExample
+//
+//  Created by hsoi on 5/7/15.
+//  Copyright (c) 2015 Hsoi Enterprises LLC. All rights reserved.
+//
+
+#import <Flurry.h>
+#import <Mixpanel.h>
+#import <GAI.h>
+#import <GAIDictionaryBuilder.h>
+#import <GAIFields.h>
