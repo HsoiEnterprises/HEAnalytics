c1c041af274fc559ac3a1d9d485533350ef37846
John C. Daub <hsoi@hsoi.com>
2015-05-07 05:39:49 -0500

    Initial project template for sample app. Adding a Podfile (and Gemfile) to help with a few things to make sample distribution buildable and a little easier to distribute. .gitignore updated to ignore the Pods folder.

---
+++ Example/HEAnalyticsExample/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HEAnalyticsExample
+//
+//  Created by hsoi on 5/7/15.
+//  Copyright (c) 2015 Hsoi Enterprises LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
