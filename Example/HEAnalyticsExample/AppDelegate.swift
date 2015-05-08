98f69fb11adc8031890ad626bad9b8545a66408e
John C. Daub <hsoi@hsoi.com>
2015-05-08 05:39:20 -0500

    Flesh out the example with a sample implementation.

--- Example/HEAnalyticsExample/AppDelegate.swift
+++ Example/HEAnalyticsExample/AppDelegate.swift
@@ -13,9 +13,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    
+    func application(application: UIApplication, willFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        SampleAnalytics.sharedInstance.start()
+        return true
+    }
+    
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        if let tabController = window?.rootViewController as? UITabBarController {
+            tabController.delegate = self
+        }
+
         return true
     }
 
@@ -41,6 +51,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
 
+
+extension AppDelegate : UITabBarControllerDelegate {
+    
+    func tabBarController(tabBarController: UITabBarController, didSelectViewController viewController: UIViewController) {
+        if tabBarController.selectedIndex == 0 {
+            SampleAnalytics.sharedInstance.selectedTab1()
+        }
+        else if tabBarController.selectedIndex == 1 {
+            SampleAnalytics.sharedInstance.selectedTab2()
+        }
+    }
+}
